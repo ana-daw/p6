08502903c87e37a7965b57f48c3786dc902e194c
Ana Belen <Ana Belen@DESKTOP-5JL2VR6>
2024-01-10 11:08:48 +0100

    commit añadir p6 java

---
+++ p6/src/main/java/com/miproyecto/p6/P6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.miproyecto.p6;
+
+/**
+ *
+ * @author Ana Belen
+ */
+public class P6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("hola");
+    }
+}
